f083dfdabfdc90308c08a96102f2eec9364e423a
suleka96 <sulekahelmini96comp@gmail.com>
2018-02-02 23:58:36 +0530

    more functions tested

--- app19.js
+++ app19.js
@@ -1,1 +1,23 @@
-//routing
+//routing 
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    console.log('request was made'+ req.url);
+    if(req.url === '/home' || req.url){
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }
+    else if(req.url === '/contact'){
+        res.writeHead(200,{})
+
+
+
+
+
+
+    }
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('yo dogs, now listening to port 3000');
